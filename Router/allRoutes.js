524eac1aada90834370007a08ea7c05898ce50e6
nedirjasmine302-bit <nedirjasmine302@gmail.com>
2026-01-30 16:30:00 +0100

    cree inscription/connextion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,9 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connextion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
 ];
+
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
