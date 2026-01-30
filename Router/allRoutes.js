e9bf51104728e31837ab2eb028c43510766caea3
nedirjasmine302-bit <nedirjasmine302@gmail.com>
2026-01-30 19:53:42 +0100

    page pour bouton reserver

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,7 @@ export const allRoutes = [
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
     new Route("/allResa", "Vos reservation", "/pages/reservation/allResa.html"),
+    new Route("/reserver", "Réserver", "/pages/reservation/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
