85ac478becd6aa5e4174b431c4412d22ec4348c5
nedirjasmine302-bit <nedirjasmine302@gmail.com>
2026-01-31 20:25:49 +0100

    limitaton d'acces au route avec les roles

--- Router/Router.js
+++ Router/Router.js
@@ -23,6 +23,21 @@ const LoadContentPage = async () => {
   const path = window.location.pathname;
   // Récupération de l'URL actuelle
   const actualRoute = getRouteByUrl(path);
+  //Vérifier les droits d'accès à la page
+  const allRolesArray = actualRoute.authorize;
+  if(allRolesArray.length > 0){
+    if(allRolesArray.includes("disconnected")){
+      if(isConnected()){
+        window.location.replace("/");
+      }
+    }
+    else{
+      const roleUser = getRole();
+      if(!allRolesArray.includes(roleUser)){
+        window.location.replace("/");
+      }
+    }
+  }
   // Récupération du contenu HTML de la route
   const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
   // Ajout du contenu HTML à l'élément avec l'ID "main-page"
