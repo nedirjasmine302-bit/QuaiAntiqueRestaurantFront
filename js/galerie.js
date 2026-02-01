35ba700b2e38ae81b23d38ca75bb639033b1b0c2
nedirjasmine302-bit <nedirjasmine302@gmail.com>
2026-02-01 15:06:34 +0100

    oublie correction xss

--- js/galerie.js
+++ js/galerie.js
@@ -9,18 +9,6 @@ let monImage = getImages(titre, imgSource);
 
 galerieImage.innerHTML = monImage;
 
-function sanitizeHtml(text){
-    // Créez un élément HTML temporaire de type "div"
-    const tempHtml = document.createElement('div');
-    
-    // Affectez le texte reçu en tant que contenu texte de l'élément "tempHtml"
-    tempHtml.textContent = text;
-    
-    // Utilisez .innerHTML pour récupérer le contenu de "tempHtml"
-    // Cela va "neutraliser" ou "échapper" tout code HTML potentiellement malveillant
-    return tempHtml.innerHTML;
-}
-
 function getImages(titre, urlImage){
     titre = sanitizeHtml(titre);
     urlImage = sanitizeHtml(urlImage);
