01c3e729cb0aca809f67faa13092f3f0d57a72eb
nedirjasmine302-bit <nedirjasmine302@gmail.com>
2026-02-02 20:45:19 +0100

    coorection route api account/me

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const RoleCookieName = "role";
 const signoutBtn = document.getElementById("signout-btn");
 
 signoutBtn.addEventListener("click", signout);
+getInfosUser();
 
 function getRole(){
     return getCookie(RoleCookieName);
@@ -94,4 +95,32 @@ function sanitizeHtml(text){
     // Utilisez .innerHTML pour récupérer le contenu de "tempHtml"
     // Cela va "neutraliser" ou "échapper" tout code HTML potentiellement malveillant
     return tempHtml.innerHTML;
+}
+
+function getInfosUser(){
+    console.log("Récupération des informations utilisateur...");
+
+    let myHeaders = new Headers();
+    myHeaders.append("X-AUTH-TOKEN", getToken());
+
+    let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+    fetch( "http://127.0.0.1:8000/api/account/me" , requestOptions)
+    .then(response =>{
+        if(response.ok){
+            return response.json();
+        }
+        else{
+            console.log("Impossible de récupérer les informations utilisateur");
+        }
+    })
+    .then(result => {
+        return result;
+    })
+    .catch(error =>{
+        console.error("erreur lors de la récupération des données utilisateur", error);
+    });
 }
