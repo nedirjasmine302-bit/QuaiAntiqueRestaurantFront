7dc7de9a638dec84564ab2cd8e4090ef741d9bd7
nedirjasmine302-bit <nedirjasmine302@gmail.com>
2026-02-02 22:31:36 +0100

    connextion avec api

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,11 +1,12 @@
 const mailInput = document.getElementById("EmailInput");
 const passwordInput = document.getElementById("PasswordInput");
 const btnSignin = document.getElementById("btnSignin");
+const signinForm = document.getElementById("signinForm");
 
 btnSignin.addEventListener("click", checkCredentials);
 
 function checkCredentials(){
-    //Ici, il faudra appeler l'API pour vérifier les credentials en BDD
+    let dataForm = new FormData(signinForm);
     
     if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
         //Il faudra récupérer le vrai token
@@ -21,4 +22,44 @@ function checkCredentials(){
         mailInput.classList.add("is-invalid");
         passwordInput.classList.add("is-invalid");
     }
+}
+
+function checkCredentials(){
+    let dataForm = new FormData(signinForm);
+    
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({
+        "username": dataForm.get("Email"),
+        "password": dataForm.get("Password")
+    });
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("http://127.0.0.1:8000/api/logi", requestOptions)
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        else{
+            mailInput.classList.add("is-invalid");
+            passwordInput.classList.add("is-invalid");
+        }
+    })
+
+    .then(result => {
+        const token = result.apiToken;
+        setToken(token);
+        //placer ce token en cookie
+        setCookie(RoleCookieName, result.roles[0], 7);
+        window.location.replace("/");
+    })
+
+    .catch(error => console.log('error', error));
 }
